6e283269cb520e14f805ffa81332a99d591c5d1a
MK192 <milann.92@hotmail.com>
2024-09-10 16:59:36 +0200

    initial setup, install dependencys, create new folders ...

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+
+  theme: {
+    screens: {
+      xl: { max: "1440px" },
+      lg: { max: "1024px" },
+      md: { max: "800px" },
+      sm: { max: "480px" },
+    },
+
+    extend: {
+      colors: {
+        blackHowl: "#1a1e2e",
+      },
+    },
+  },
+  plugins: [],
+};
